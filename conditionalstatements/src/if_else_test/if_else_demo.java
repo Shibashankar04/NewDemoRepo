b7395e153b2157a6906d5aba30684ccac24d0198
pc <pc@DESKTOP-T8VQ6H9>
2019-04-04 10:45:28 +0530

    Local Repo First commit

---
+++ conditionalstatements/src/if_else_test/if_else_demo.java
@@ -0,0 +1,37 @@
+package if_else_test;
+
+public class if_else_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//aa:
+		for (int x=30;x>=20;x--)
+		//{
+			//do{
+			//bb:	
+			for (int y=20;y<=30;y++)
+			{
+			//} while(y!=x);
+		
+		if (x>y)
+		{
+			System.out.println("x is greater");
+		}
+		else if(x<y)
+		{
+		    System.out.println("y is greater");	
+		}
+		else
+		{
+			System.out.println("they are equal");
+		}
+		// break aa;
+		//} while(y!=x);
+		
+		//}
+		}
+		}
+
+
+}
